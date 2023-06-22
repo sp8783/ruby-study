7618937a65989e2b5bde688d7f01b966ec6b1b4d
sp8783 <p.sp8783@gmail.com>
2023-06-22 14:17:41 +0900

    add programs for the example problem in Chapter 10

---
+++ cherry_book/example/lib/effects.rb
@@ -0,0 +1,22 @@
+module Effects
+  def self.reverse
+    ->(words) do
+      words.split(' ').map(&:reverse).join(' ')
+    end
+  end
+
+  def self.echo(rate)
+    ->(words) do
+      # スペースならそのまま返す
+      # スペース以外ならその文字を指定された回数だけ繰り返す
+      words.each_char.map { |c| c == ' ' ? c : c * rate }.join
+    end
+  end
+
+  def self.loud(level)
+    ->(words) do
+    # スペースで分割 > 大文字変換と"!"の付与 > スペースで連結
+    words.split(' ').map { |word| word.upcase + '!' * level }.join(' ')
+    end
+  end
+end
